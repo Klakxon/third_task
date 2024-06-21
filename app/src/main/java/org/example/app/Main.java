451c998d86c67b572083b270c14f1005bfd1d8b6
Viktor Kyshencuk <v.kyshenchuk@ukma.edu.ua>
2024-06-21 23:50:01 +0300

    first commit

---
+++ app/src/main/java/org/example/app/Main.java
@@ -0,0 +1,7 @@
+package org.example.app;
+
+public class LibraryClass {
+    public static String getGreeting() {
+        return "Hello from Library!";
+    }
+}
---
+++ utils/src/main/java/org/example/utils/Utils.java
@@ -0,0 +1,7 @@
+package org.example.library;
+
+public class Utils {
+    public static String getUtility() {
+        return "Utility Method";
+    }
+}
